cac585fb0b4c4862c85cf2cc996c95a59d9c6bb3
edgarcarlos <125299013+edgarcarlos@users.noreply.github.com>
2025-01-12 11:03:54 +0100

    Update gulpease.py

--- .github/gulpease.py
+++ .github/gulpease.py
@@ -77,7 +77,7 @@ def gulpease(doc:str) -> tuple[int, int]:
     current_path = path.Path(doc)
     tot=0
     n_files=0
-    for file in os.listdir(current_path): # for each file in a document
+    for file in os.listdir(current_path): # for each file in a doc
         if is_checkable(file): 
             n_files+=1
             idx = calculate_gulpease(str(current_path) + "/" + file)
